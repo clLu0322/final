519a88d85e32bf32956b35cc65dbc4378de7ae9f
clayshaw <howardchen1222@gmail.com>
2024-01-02 12:08:28 +0800

    add plane

--- final/final/plane.h
+++ final/final/plane.h
@@ -1,1 +1,34 @@
 #pragma once
+#ifndef PLANE
+#define PLANE
+
+typedef struct point
+{
+	int x;
+	int y;
+}point;
+
+#define WIDTH_PALEN 20
+#define HEIGHT_PLANE 20
+#define ENEMY 5
+
+bool gameOver_Plane;
+point plane;
+point enemy;
+point bullet;
+int score_Plane;
+double enemy_speed;
+short level;
+
+enum Direction_Plane { STOP_PLANE = 0, LEFT_PLANE, RIGHT_PLANE, SHUT };
+enum BulletState { NO_BULLET, HAVE_BULLET };
+enum Direction_Plane dir_plane;
+enum BulletState bull;
+
+void Plane();
+void Logic_plane();
+void Input_plane();
+void Draw_plane();
+void Setup_plane();
+
+#endif // !PLANE
